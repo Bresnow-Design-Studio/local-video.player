4cfad611739cae903016432f877ba176af1a42ec
Gustavo A <atencio361@hotmail.com>
2021-09-07 17:37:11 -0500

    Old styles for buttons to navigate in video player

--- src/pages/videoPlayer.ts
+++ src/pages/videoPlayer.ts
@@ -37,6 +37,16 @@ const videoPlayer = (videoPath: string): string => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>${video?.series} - Home Video Player</title>
       <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      <script src="https://cdn.jsdelivr.net/npm/css-vars-ponyfill@2"></script>
+      <script type="text/javascript">
+        cssVars()
+      </script>
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"
+        integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu"
+        crossorigin="anonymous"
+      />
       ${css}
     </head>
     <body>
@@ -45,10 +55,13 @@ const videoPlayer = (videoPath: string): string => {
         <video class="video-player" controls autoplay preload="auto">
           <source src="${video?.path}"></source>
         </video>
-        <div class="video-links">
-          ${video?.previous ? previousVideoLink(video) : ''}
-          ${video?.next ? nextVideoLink(video) : ''}
-        </div>
+        <table class="video-links">
+          <tr>
+            <td>${video?.previous ? previousVideoLink(video) : ''}</td>
+            <td><a href="/" class="fas fa-home home-button" title="Home"></a></td>
+            <td>${video?.next ? nextVideoLink(video) : ''}</td>
+          </tr>
+        </table>
       </div>
     </body>
   </html>
